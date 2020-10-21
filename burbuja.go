d8280a94f1707bb1031fbb49c3b375c79e0a01ef
Alexis Guzmán <alegu@Alexiss-MacBook-Pro.local>
2020-10-20 21:01:33 -0500

    first commit

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,26 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func swap(s []int64, i int, j int) {
+	temp := s[i]
+	s[i] = s[j]
+	s[j] = temp
 }
 
-func main()  {
-	
+func Burbuja(s []int64) {
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s)-i-1; j++ {
+			if s[j] > s[j+1] {
+				swap(s, j, j+1)
+			}
+		}
+	}
+}
+
+func main() {
+	var s = []int64{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 10, 11}
+
+	Burbuja(s)
+	fmt.Print(s)
 }
